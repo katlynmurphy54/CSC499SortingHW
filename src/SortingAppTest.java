3d30a24a5af16f221d711c0b6f23f01c74465352
Katlyn Murphy <73905914+katlynmurphy54@users.noreply.github.com>
2021-04-06 21:48:31 -0400

    Add files via upload

---
+++ src/SortingAppTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+class SortingAppTest {
+	
+	@Test
+	void testBasicInput() throws IOException { //basic test for sorting app NO REVERSE
+        //you need to change the test to throw IOException in case it does not run
+		//you also need to initialize the output file and create an expected file 
+		//so the output file and expected file can compare
+		
+		String outputFile = "C:\\Users\\katly\\Desktop\\output.txt"; 
+        SortingApp.main(null);
+        String expectedFile = "C:\\Users\\katly\\Desktop\\expected.txt"; 
+        assertEquals(expectedFile, outputFile);
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	void testBasicInputWithReverse() throws IOException { //basic test for sorting app with
+		//you need to change the test to throw IOException in case it does not run
+		//you also need to initialize the output file and create an expected file 
+		//so the output file and expected file can compare
+		String outputFile = "C:\\Users\\katly\\Desktop\\output.txt"; 
+		SortingApp.main(null);
+		String expectedFile = "C:\\Users\\katly\\Desktop\\expected.txt"; 
+		assertEquals(expectedFile, outputFile);
+		//fail("Not yet implemented");
+	}
+	
+}
